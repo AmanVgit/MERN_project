0427b70b78a275208742db0bfb6aec357f1144a4
aman verma <9.amanverma1@gmail.com>
2023-06-04 00:27:35 +0530

    Made Home page along with loading spinner

--- client/src/components/tables/Tables.js
+++ client/src/components/tables/Tables.js
@@ -1,8 +1,70 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Badge from 'react-bootstrap/Badge'
+import "./table.css"
 
 const Tables = () => {
   return (
-    <div>Tables</div>
+    <>
+      <div className="container">
+        <Row>
+          <div className="col mt-0">
+            <Card className="shadow">
+            <Table className="align-align-items-center" responsic="sm">
+                <thead className='thead-dark'>
+                  <tr className='table-dark'>
+                    <th>ID</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                    <th>Status</th>
+                    <th>Profile</th>
+                    <th>Action</th>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Aman verma</td>
+                    <td>aman@gmail.com</td>
+                    <td>M</td>
+                    <td className='d-flex align-items-center'>
+                    <Dropdown className='text-center'>
+                <Dropdown.Toggle className='dropdown_btn' id="dropdown-basic">
+                  <Badge bg='primary'>
+                    Active <i class="fa-solid fa-angle-down"></i>
+                  </Badge>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item>Active</Dropdown.Item>
+                  <Dropdown.Item>Inactive</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+                    </td>
+                    <td className='img_parent'>
+                      <img src="/man.png" alt="" />
+                    </td>
+                    <td className='text-center'>
+                    <Dropdown className='text-center'>
+                <Dropdown.Toggle variant='light' className='action' id="dropdown-basic">
+                  <i class="fa-solid fa-ellipsis-vertical"></i>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item><i class="fa-solid fa-eye" style={{color:"green"}}></i> View</Dropdown.Item>
+                  <Dropdown.Item><i class="fa-solid fa-pen-to-square" style={{color:"blue"}}></i> Edit</Dropdown.Item>
+                  <Dropdown.Item><i class="fa-sharp fa-solid fa-trash" style={{color:"red"}}></i> Delete</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+                    </td>
+                  </tr>
+                </thead>
+              </Table> 
+            </Card>
+          </div>
+        </Row>
+      </div>
+    </>
   )
 }
 
--- client/src/pages/Edit/Edit.js
+++ client/src/pages/Edit/Edit.js
@@ -4,12 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Select from 'react-select';
+import Spiner from '../../components/spiner/Spiner'
 import { ToastContainer, toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 import "./edit.css"
 
 const Edit = () => {
 
+  const [showspin, setShowSpin] = useState(true);
   const [inputdata, setInputData] = useState({
     fname: "",
     lname: "",
@@ -54,6 +56,9 @@ const Edit = () => {
       setPreview(URL.createObjectURL(image))
       console.log(image);
     }
+    setTimeout(() => {
+      setShowSpin(false)
+    }, 500)
   }, [image])
 
   //submit userdata
@@ -62,114 +67,117 @@ const Edit = () => {
 
     //on submitting form lets just validate out entry
     const { fname, lname, email, mobile, gender, location } = inputdata;
-      
-      if(fname===""){
-        toast.error("First name is required!");
-      }else if(lname===""){
-        toast.error("Last name is required!")
-      }else if(email===""){
-        toast.error("email is required!")
-      }else if(!email.includes("@")){
-        toast.error("Enter valid Email!")
-      }else if(mobile===""){
-        toast.error("Mobile is required!")
-      }else if(mobile.length>10){
-        toast.error("Enter valid Mobile number!")
-      }else if(gender===""){
-        toast.error("Select Gender")
-      }else if(status===""){
-        toast.error("Select Status")
-      }else if(image===""){
-        toast.error("Profile is Required")
-      }else if(location===""){
-        toast.error("Location is Required")
-      }else{
-        toast.success("Registration succesfully done!")
-      }
+
+    if (fname === "") {
+      toast.error("First name is required!");
+    } else if (lname === "") {
+      toast.error("Last name is required!")
+    } else if (email === "") {
+      toast.error("email is required!")
+    } else if (!email.includes("@")) {
+      toast.error("Enter valid Email!")
+    } else if (mobile === "") {
+      toast.error("Mobile is required!")
+    } else if (mobile.length > 10) {
+      toast.error("Enter valid Mobile number!")
+    } else if (gender === "") {
+      toast.error("Select Gender")
+    } else if (status === "") {
+      toast.error("Select Status")
+    } else if (image === "") {
+      toast.error("Profile is Required")
+    } else if (location === "") {
+      toast.error("Location is Required")
+    } else {
+      toast.success("Registration succesfully done!")
+    }
   }
   return (
     <>
-      <div className="container">
-        <h2 className='text-center mt-1'>Update Your Details</h2>
-        <Card className='shadow mt-3 p-3'>
-          <div className="profile_div text-center">
-            <img src={preview ? preview : "/man.png"} alt="img" />
-          </div>
-          <Form>
-            <Row>
+      {
+        showspin ? <Spiner /> : <div className="container">
+          <h2 className='text-center mt-1'>Update Your Details</h2>
+          <Card className='shadow mt-3 p-3'>
+            <div className="profile_div text-center">
+              <img src={preview ? preview : "/man.png"} alt="img" />
+            </div>
+            <Form>
+              <Row>
 
-              {/* first name */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter First Name" name="fname" value={inputdata.fname} onChange={setInputValue} />
-              </Form.Group>
+                {/* first name */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter First Name" name="fname" value={inputdata.fname} onChange={setInputValue} />
+                </Form.Group>
 
-              {/* last name */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter last Name" name="lname" value={inputdata.lname} onChange={setInputValue} />
-              </Form.Group>
+                {/* last name */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter last Name" name="lname" value={inputdata.lname} onChange={setInputValue} />
+                </Form.Group>
 
-              {/* email */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="email" value={inputdata.email} onChange={setInputValue} />
-              </Form.Group>
+                {/* email */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" name="email" value={inputdata.email} onChange={setInputValue} />
+                </Form.Group>
 
-              {/* mobile */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Mobile</Form.Label>
-                <Form.Control type="email" placeholder="Enter Mobile number" name="mobile" value={inputdata.mobile} onChange={setInputValue} />
-              </Form.Group>
+                {/* mobile */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Mobile</Form.Label>
+                  <Form.Control type="email" placeholder="Enter Mobile number" name="mobile" value={inputdata.mobile} onChange={setInputValue} />
+                </Form.Group>
 
-              {/* gender */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Select your Gender</Form.Label>
-                <Form.Check
-                  type={"radio"}
-                  label={"Male"}
-                  name="gender"
-                  value={"Male"}
-                  onChange={setInputValue}
-                />
-                <Form.Check
-                  type={"radio"}
-                  label={"Female"}
-                  name="gender"
-                  value={"Female"}
-                  onChange={setInputValue}
-                />
-              </Form.Group>
+                {/* gender */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select your Gender</Form.Label>
+                  <Form.Check
+                    type={"radio"}
+                    label={"Male"}
+                    name="gender"
+                    value={"Male"}
+                    onChange={setInputValue}
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={"Female"}
+                    name="gender"
+                    value={"Female"}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
 
-              {/* Status */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Select your Status</Form.Label>
-                <Select
-                  options={options}
-                  value={status}
-                  onChange={setStatusValue}
-                />
-              </Form.Group>
+                {/* Status */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select your Status</Form.Label>
+                  <Select
+                    options={options}
+                    value={status}
+                    onChange={setStatusValue}
+                  />
+                </Form.Group>
 
-              {/* Profile */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Select your Profile</Form.Label>
-                <Form.Control type="file" name="user_profile" onChange={setProfile} />
-              </Form.Group>
+                {/* Profile */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select your Profile</Form.Label>
+                  <Form.Control type="file" name="user_profile" onChange={setProfile} />
+                </Form.Group>
 
-              {/* location   */}
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Select your Location</Form.Label>
-                <Form.Control type="Tetx" placeholder="Enter your Location" name="location" onChange={setInputValue} />
-              </Form.Group>
-              <Button variant="primary" type="submit" value={inputdata.location} onClick={submitUserData}>
-                Submit
-              </Button>
-            </Row>
-          </Form>
-        </Card>
-        <ToastContainer position="top-center" />
-      </div>
+                {/* location   */}
+                <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                  <Form.Label>Select your Location</Form.Label>
+                  <Form.Control type="Tetx" placeholder="Enter your Location" name="location" onChange={setInputValue} />
+                </Form.Group>
+                <Button variant="primary" type="submit" value={inputdata.location} onClick={submitUserData}>
+                  Submit
+                </Button>
+              </Row>
+            </Form>
+          </Card>
+          <ToastContainer position="top-center" />
+        </div>
+      }
+
     </>
   )
 }

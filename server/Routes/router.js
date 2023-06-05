b3bb5646ace962709b5eb02e0b9b2934a1b739c9
aman verma <9.amanverma1@gmail.com>
2023-06-05 14:11:31 +0530

    Started with backend--created setup files

---
+++ server/Routes/router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = new express.Router();
+const controllers = require("../Controllers/usersControllers");
+const upload = require("../multerconfig/storageConfig")
+
+//routes
+router.post("/user/register",upload.single("user_profile"),controllers.userpost)
+
+
+
+module.exports = router  
